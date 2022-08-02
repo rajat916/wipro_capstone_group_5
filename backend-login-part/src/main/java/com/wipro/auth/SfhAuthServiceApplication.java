780a5819ed7508694fbd0625742ad6e59405c877
rajat916 <rajatpancholi35@gmail.com>
2022-08-02 10:23:03 +0530

    project deployed

---
+++ backend-login-part/src/main/java/com/wipro/auth/SfhAuthServiceApplication.java
@@ -0,0 +1,15 @@
+package com.wipro.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@SpringBootApplication
+
+public class SfhAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfhAuthServiceApplication.class, args);
+	}
+
+}
